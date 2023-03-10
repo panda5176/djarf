faa1c062cb866d4c81db4adad9b1d24253f96aff
panda5176 <panda5176@gmail.com>
2023-03-10 22:13:45 +0900

    Using Routers

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,46 +1,12 @@
 from django.urls import path, include
-from rest_framework.renderers import StaticHTMLRenderer
-from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 from snippets import views
 
+router = DefaultRouter()
+router.register(r"snippets", views.SnippetViewSet, basename="snippet")
+router.register(r"users", views.UserViewSet, basename="user")
+
 urlpatterns = [
-    path("", views.api_root),
-    path(
-        "snippets/",
-        views.SnippetViewSet.as_view({"get": "list", "post": "create"}),
-        name="snippet-list",
-    ),
-    path(
-        "snippets/<int:pk>/",
-        views.SnippetViewSet.as_view(
-            {
-                "get": "retrieve",
-                "put": "update",
-                "patch": "partial_update",
-                "delete": "destroy",
-            }
-        ),
-        name="snippet-detail",
-    ),
-    path(
-        "snippets/<int:pk>/highlight/",
-        views.SnippetViewSet.as_view(
-            {"get": "highlight"}, renderer_classes=[StaticHTMLRenderer]
-        ),
-        name="snippet-highlight",
-    ),
-    path(
-        "users/", views.UserViewSet.as_view({"get": "list"}), name="user-list"
-    ),
-    path(
-        "users/<int:pk>/",
-        views.UserViewSet.as_view({"get": "retrieve"}),
-        name="user-detail",
-    ),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
-
-urlpatterns += [
+    path("", include(router.urls)),
     path("api-auth/", include("rest_framework.urls")),
 ]
