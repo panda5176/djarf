4b8ad8826cf43bc31c4b22b29e1fd956e7491956
panda5176 <panda5176@gmail.com>
2023-03-10 20:53:25 +0900

    Adding pagination

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,12 +3,16 @@ from rest_framework import serializers
 from snippets.models import Snippet
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name="snippet-highlight", format="html"
+    )
 
     class Meta:
         model = Snippet
         fields = [
+            "url",
             "id",
             "title",
             "code",
@@ -16,14 +20,15 @@ class SnippetSerializer(serializers.ModelSerializer):
             "language",
             "style",
             "owner",
+            "highlight",
         ]
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Snippet.objects.all()
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedIdentityField(
+        many=True, view_name="snippet-detail", read_only=True
     )
 
     class Meta:
         model = User
-        fields = ["id", "username", "snippets"]
+        fields = ["url", "id", "username", "snippets"]
