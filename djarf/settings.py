81a0beac2a7e3e768a9a0161ff4f05cc619b6381
panda5176 <panda5176@gmail.com>
2023-04-27 21:42:02 +0900

    RDS test

--- djarf/settings.py
+++ djarf/settings.py
@@ -93,6 +93,18 @@ DATABASES = {
     }
 }
 
+if os.environ.get("DJARF_PROD"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": "djarf",
+            "USER": "admin",
+            "PASSWORD": os.environ["DB_PASSWORD"],
+            "HOST": os.environ["DB_HOST"],
+            "PORT": "3306",
+            "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
